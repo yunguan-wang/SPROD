1ff6fd1499c191557e1b10b4fc1d0b8dd03d815f
yunguan-wang <drizzter.yw@gmail.com>
2021-09-17 15:36:44 -0500

    removed unnecessary codes

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ requires = [
     "numpy",
     "scipy",
     "scikit-learn",
-    "hdbscan"
+    "hdbscan",
+    "tables"
 ]
 
 DESCRIPTION = "De-noising of Spatial Expression Profiling Data Based on Latent Graph Learning of in situ Position and Image Data"
