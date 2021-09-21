375050d5a32590068fdc8f140f210052a2607357
yunguan-wang <drizzter.yw@gmail.com>
2021-09-21 13:58:40 -0500

    bug fix

--- sprod/denoise_job.py
+++ sprod/denoise_job.py
@@ -451,7 +451,7 @@ if __name__ == "__main__":
                 sprod_margin,
                 sprod_umap,
                 sprod_diag,
-                sprod_path.replace('/sprod',''),
+                '/'.join(sprod_path.split('/')[:-1]),
                 os.path.abspath(output_path),
                 patch_name,
             ],
